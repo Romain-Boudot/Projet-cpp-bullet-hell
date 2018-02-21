35bb1efa6cc6136d9dd4c453cb575ab2bc67065e
YumYumi <romain.boudot@epsi.fr>
2018-02-21 13:46:38 +0100

    test

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
-#include <iostream>
-#include <thread>
+#include <SFML/Graphics.hpp>
 
-using namespace std;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(500, 800), "SFML works!", sf::Style::Fullscreen);
+    window.setFramerateLimit(60); // framerate
+    sf::CircleShape shape(100.f,100);
+    shape.setFillColor(sf::Color::Green);
 
-int main() {
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-    cout << "sa marche" << endl;
+        sf::Vector2i mouse_posi = sf::Mouse::getPosition(window);
+        sf::Vector2f mouse_posf;
+
+        mouse_posf.x = ((int) mouse_posi.x) - shape.getRadius();
+        mouse_posf.y = ((int) mouse_posi.y) - shape.getRadius();
+        
+        shape.setPosition(mouse_posf);
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
     return 0;
-
 }
