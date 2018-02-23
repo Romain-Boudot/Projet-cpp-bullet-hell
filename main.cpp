ff12c47a0aef6e274d6d393cd63443575f00bf6a
YumYumi <romain.boudot@epsi.fr>
2018-02-23 16:55:52 +0100

    thread d'affichage

--- main.cpp
+++ main.cpp
@@ -1,68 +1,66 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <iostream>
+#include <vector>
 
-class Param {
+class Bullet_hell {
     public:
+        int windowHeight;
+        int windowWidth;
         sf::RenderWindow *window;
-        sf::CircleShape *shape;
+        sf::CircleShape *player_sprite;
 };
 
-void thread_aff(Param param) {
+void thread_aff(Bullet_hell *game) { // thread d'affichage
 
-    sf::RenderWindow *window = param.window;
-    sf::CircleShape *shape = param.shape;
+    sf::RenderWindow window(sf::VideoMode(500, 800), "SFML works!");
+    window.setFramerateLimit(60); // framerate
 
-    std::cout << "thread aff" << std::endl;
+    game->window = &window;
+    sf::CircleShape *player_sprite = game->player_sprite;
 
-    window->create(sf::VideoMode(500, 800), "SFML works!");
+    window.setMouseCursorVisible(false);
 
-    while (window->isOpen()) {
+    while (window.isOpen()) {
+
+        
         sf::Event event;
-        while (window->pollEvent(event))
-        {
+
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
-                window->close();
+                window.close();
         }
 
-        sf::Vector2i mouse_posi = sf::Mouse::getPosition(*window);
+        sf::Vector2i mouse_posi = sf::Mouse::getPosition(window);
         sf::Vector2f mouse_posf;
 
-        mouse_posf.x = ((int) mouse_posi.x) - shape->getRadius();
-        mouse_posf.y = ((int) mouse_posi.y) - shape->getRadius();
+        mouse_posf.x = ((int) mouse_posi.x) - player_sprite->getRadius();
+        mouse_posf.y = ((int) mouse_posi.y) - player_sprite->getRadius();
 
-        shape->setPosition(mouse_posf);
+        player_sprite->setPosition(mouse_posf);
 
-        window->clear();
-        window->draw(*shape);
-        window->display();
-
+        window.clear(sf::Color(0, 0, 30, 200));
+        window.draw(*player_sprite);
+        window.display();
 
     }
 
 }
 
-int main() {
-
-    sf::RenderWindow window(sf::VideoMode(500, 800), "SFML works!");
-    window.setFramerateLimit(60); // framerate
-    sf::CircleShape shape(100.f,100);
-    shape.setFillColor(sf::Color::Green);
-
-    Param param;
-
-    param.shape = &shape;
-    param.window = &window;
-
-    std::cout << "main thread" << std::endl;
-
+int main() {    
     
+    sf::CircleShape player_sprite(5.f,100);
+    player_sprite.setFillColor(sf::Color::Green);
 
-    sf::Thread thread1(&thread_aff, param);
+    Bullet_hell game;
+
+    game.player_sprite = &player_sprite;
+
+    sf::Thread th_aff(&thread_aff, &game);
     
-    thread1.launch();
+    th_aff.launch();
 
-    thread1.wait();
+    th_aff.wait();
 
     return 0;
 }
