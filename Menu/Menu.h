cc9c6d8cba56dfb121a08b4d5f0f04fcc01e4e23
camillebourget <camillebourgetsen@gmail.com>
2018-03-12 17:11:42 +0100

    MAG

---
+++ Menu/Menu.h
@@ -0,0 +1,30 @@
+//
+// Created by camillebourget on 12/03/18.
+//
+
+#ifndef PROJET_CPP_BULLET_HELL_MENU_H
+#define PROJET_CPP_BULLET_HELL_MENU_H
+
+#endif //PROJET_CPP_BULLET_HELL_MENU_H
+#pragma once
+#include "SFML/Graphics.hpp"
+
+#define MAX_NUMBER_OF_ITEMS 3
+
+class Menu
+{
+public:
+    Menu(float width, float height);
+    ~Menu();
+
+    void draw(sf::RenderWindow &window);
+    void MoveUp();
+    void MoveDown();
+    int GetPressedItem() { return selectedItemIndex; }
+
+private:
+    int selectedItemIndex;
+    sf::Font font;
+    sf::Text menu[MAX_NUMBER_OF_ITEMS];
+
+};
